fd4a947b4791e30e066887d2d2cc7a6788edd8c1
tushar-saini31 <ts2475131@gmail.com>
2025-11-20 22:27:01 +0530

    updations

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,84 +1,87 @@
-const mongoose=require("mongoose")
-const validator=require("validator");
+const mongoose = require("mongoose");
+const validator = require("validator");
 const bcrypt = require("bcrypt");
-const jwt=require("jsonwebtoken");
-const userSchema= new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        index:true,
-        minLength:3,
-        maxlenth:50
-    }, 
-    lastName:{
-        type: String
+const jwt = require("jsonwebtoken");
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    index: true,
+    minLength: 3,
+    maxlenth: 50,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email address" + value);
+      }
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true,
-        validate(value){
-            if(!validator.isEmail(value))
-            {
-                throw new Error("Invalid email address"+value)
-            }
-        }
-    }, 
-    password:{
-        type:String, 
-        required:true,
-        validate(value){
-            if(!validator.isStrongPassword(value))
-            {
-                throw new Error("Invalid email address"+value)
-            }
-        }
-    }, 
-    age:{
-        type:Number
-    }, 
-    gender:{
-        type:String,
-        enum:{
-            values:["male", "female", "other"],
-            message:'{value} is not valid gender type'
-        },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Invalid email address" + value);
+      }
     },
-    about:{
-        type:String,
-        default:"this is dafault about the user"
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ["male", "female", "other"],
+      message: "{value} is not valid gender type",
     },
-    photoUrl:{
-        type:String,
-        default: "https://cdn-icons-png.flaticon.com/512/847/847969.png"
+  },
+  about: {
+    type: String,
+    default: "this is dafault about the user",
+  },
+  // photoUrl:{
+  //     type:String,
+  //     default: "https://cdn-icons-png.flaticon.com/512/847/847969.png"
 
-    },
-    isPremium:{
-      type: Boolean,
-      default: false
-    },
-    membershipType:{
-      type: String,
-    }
-
+  // },
+  photoUrl: {
+    type: String, // this will store either a URL or base64 string
+    default: "https://cdn-icons-png.flaticon.com/512/847/847969.png",
+  },
+  photoType: {
+    type: String, // will store image MIME type (e.g., 'image/png')
+  },
+  isPremium: {
+    type: Boolean,
+    default: false,
+  },
+  membershipType: {
+    type: String,
+  },
 });
-userSchema.methods.getJWT=async function(){
-    const user =this;
-    const token=await jwt.sign({_id:user._id},"DEV@TINDER$790",{
-        expiresIn:"7d",
-    });
-    return token;
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "DEV@TINDER$790", {
+    expiresIn: "7d",
+  });
+  return token;
 };
-userSchema.methods.validatePassword=async function(passwordInputByUser)
-{
-    const user=this;
-    const passwordHash=user.password;
-    const ispasswordValid=await bcrypt.compare(
-        passwordInputByUser,
-        passwordHash
-    )
-    return ispasswordValid;
-}
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+  const ispasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+  return ispasswordValid;
+};
 
-module.exports=mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,18 +3,25 @@ const profileRouter=express.Router();
 const {UserAuth}=require("../middleware/auth");
 const { validateEditProfileData } = require("../utils/validation");
 
-profileRouter.get("/profile/view",UserAuth,  async(req, res)=>{
-    try{
-    const user=req.user;
-    if(!user){
-        throw new Error("user does not exist");
+
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
+
+
+profileRouter.get("/profile/view", UserAuth, async (req, res) => {
+  try {
+    const userId = req.user._id; // get ID from auth middleware
+    const freshUser = await User.findById(userId); // fetch latest user
+    if (!freshUser) {
+      throw new Error("User does not exist");
     }
-    res.send(user);
-}catch(err)
-{
-    res.status(400).send("error"+err.message);
-}
+
+    res.json(freshUser); // send updated data including photoUrl
+  } catch (err) {
+    res.status(400).send("error: " + err.message);
+  }
 });
+
 
 profileRouter.patch("/profile/edit", UserAuth, async (req, res)=>{
     try{
@@ -32,6 +39,33 @@ profileRouter.patch("/profile/edit", UserAuth, async (req, res)=>{
        res.status(400).send("error"+err.message);
     }
 })
+
+// Upload profile photo
+profileRouter.post("/profile/upload", UserAuth, upload.single("photo"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    const user = req.user;
+    const imageBase64 = req.file.buffer.toString("base64");
+    const imageType = req.file.mimetype;
+
+    user.photoUrl = `data:${imageType};base64,${imageBase64}`;
+    user.photoType = imageType;
+
+    await user.save();
+
+    const updatedUser = await User.findById(user._id); // fresh data from MongoDB
+
+    // ✅ send structured response
+    res.json({ message: "Profile photo uploaded successfully", data: updatedUser });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
 
 
 module.exports=profileRouter;
